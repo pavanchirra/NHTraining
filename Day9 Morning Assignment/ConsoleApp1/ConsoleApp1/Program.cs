5ce13882a22fe8be6301947839ceb92965c5135d
pavanchirra <pa1kumar.chirra@gmail.com>
2022-02-03 20:09:57 +0530

    PPT ON ARRAYS

---
+++ Day9 Morning Assignment/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace BubbleSort
+{
+    class MySort
+    {
+        static void Main(string[] args)
+        {
+            int[] data = { 78, 55, 45, 98, 13 };
+            int temp;
+            for (int j = 0; j <= data.Length - 2; j++)
+            {
+                for (int i = 0; i <= data.Length - 2; i++)
+                {
+                    if (data[i] > data[i + 1])
+                    {
+                        temp = data[i + 1];
+                        data[i + 1] = data[i];
+                        data[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted:");
+            foreach (int d in data)
+                Console.Write(d + " ");
+            Console.Read();
+        }
+    }
+}
