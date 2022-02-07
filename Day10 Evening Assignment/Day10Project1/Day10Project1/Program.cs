7d4933717c63cd64e701299fd6701e5862697f0a
pavanchirra <pa1kumar.chirra@gmail.com>
2022-02-07 20:07:47 +0530

    Assignment

---
+++ Day10 Evening Assignment/Day10Project1/Day10Project1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+
+// DONE BY : PAVAN
+// Purpose :  Creation OF Abstract Template
+
+
+namespace Day10EVEProject1
+{
+    abstract class Salary
+    {
+        /// <summary>
+        /// This is a GetPF Normal Method with Body
+        /// </summary>
+        /// <param name="basic">INT Basic</param>
+        /// <returns>Cal O/p</returns>
+        public int GetPF(int basic)
+        { return 12 * basic / 100; }
+        /// <summary>
+        /// This is a Get HRA Normal Method with Body
+        /// </summary>
+        /// <param name="basic">INT Baisc</param>
+        /// <returns>Cal O/p</returns>
+        public int GetHRA(int basic)
+        { 
+            return 40 * basic / 100;
+        }
+
+        public abstract int GetCA();
+        /// <summary>
+        /// This is an Abstract Method GetSA, it doesn't have a Body in Abstract class.
+        /// </summary>
+        /// <returns>Enforcing Derived class to Use this method mandatorly.</returns>
+        public abstract int GetSA();
+    }
+    
+    class Microsoft : Salary
+    {
+
+        public override int GetCA()
+        {
+            return 9000;
+        }
+
+        public override int GetSA()
+        {
+            return 6000;
+        }
+    }
+    class Google : Salary
+    {
+        public override int GetCA()
+        {
+            return 8000;
+        }
+
+        public override int GetSA()
+        {
+            return 5000;
+        }
+    }
+    class IBM : Salary
+    {
+        public override int GetCA()
+        {
+            return 4000;
+        }
+
+        public override int GetSA()
+        {
+            return 6000;
+        }
+    }
+    class Facebook : Salary
+    {
+        public override int GetCA()
+        {
+            return 5000;
+        }
+
+        public override int GetSA()
+        {
+            return 1000;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("TRANSACTION SUCCESSFUL");
+
+            Console.ReadLine();
+        }
+    }
+}
--- Day11 Assignment/Day11Project4/Day11Project4/Program.cs
+++ Day11 Assignment/Day11Project4/Day11Project4/Program.cs
@@ -8,37 +8,10 @@ namespace Day11Project3
 {
     class Employee
     {
-        private int id;
-        private string name;
-        private string designation;
-        public int Id
-        {
-            get { return id; }
-            set { id = value; }
-        }
-        public string Name
-        {
-            get => name;
-            set { name = value; }
-        }
-        public string Designation
-        {
-            set { designation = value; }
-        }
-        public int Salary
-        {
-            get
-            {
-                if (designation == "M")
-                    return 90000;
-                else if (designation == "EMP")
-                    return 50000;
-                else if (designation == "SECURITY")
-                    return 75000;
-                else
-                    return 30000;
-            }
-        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Designation { get; set; }
     }
     internal class Program
     {
@@ -48,20 +21,11 @@ namespace Day11Project3
             Employee e = new Employee();
             e.Id = 20;
             e.Name = "JK";
-            e.Designation = "TL";
-            Console.WriteLine($"{e.Id},{e.Name},{e.Salary}");
+            e.Salary = 80000;
+            e.Designation = "MANAGER";
+            Console.WriteLine($"{e.Id},{e.Name},{e.Salary},{e.Designation}");
 
-            Employee emp1 = new Employee();
-            emp1.Id = 876;
-            emp1.Name = "NARESH";
-            emp1.Designation = "EMP";
-            Console.WriteLine($"{emp1.Id},{emp1.Name}, {emp1.Salary}");
-
-            Employee emp2 = new Employee();
-            emp2.Id = 222;
-            emp2.Name = "SINGH";
-            emp2.Designation = "SECURITY";
-            Console.WriteLine($"{emp2.Id},{emp2.Name},{emp2.Salary}");
+           
 
             Console.ReadLine();
         }
