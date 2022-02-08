6084307cd38f3ca91e6d855df7fe0fd97c7c3ed0
pavanchirra <pa1kumar.chirra@gmail.com>
2022-02-08 20:18:54 +0530

    Day 12 Assignment

---
+++ Day12 Assignment/Day12Project1/Day12Project1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12Project1
+{
+    /// <summary>
+    /// DONE BY: PAVAN
+    /// PURPOSE: A SIMPLE DIVISION PROGRAM USING EXCEPTION HANDLING:
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int a, b, c;
+                Console.WriteLine("Enter first number:");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter second number:");
+                b = Convert.ToInt32(Console.ReadLine());
+                c = a / b;
+                Console.WriteLine("{0}", c);
+                Console.ReadLine();
+            }
+            //USING OVERFLOW EXCEPTION//
+            catch (OverflowException)
+            {
+                Console.WriteLine("Enter the integer value from 0 to 50000: ");
+            }
+            //USING DIVIDE BY ZERO EXCEPTION//
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("Enter the integer value greater than Zero: ");
+            }
+            //USING FORMAT EXCEPTION
+            catch(FormatException)
+            {
+                Console.WriteLine(" Please Enter only integer value");
+            }
+            //USING GENERAL EXCEPTION//
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to display Output please contact admin@nbht.com");
+                Console.ReadLine();
+            }
+        }
+        
+    }
+}
---
+++ Day12 Assignment/Day12Project2/Day12Project2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12Project2
+{
+    /// <summary>
+    /// DONE BY: PAVAN//
+    /// PURPOSE: Use of "finally" block illustrate with an example.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// USING DIVIDEBYZERO EXCEPTION METHOD//
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int a, b, c, d, e;
+            Console.WriteLine("Enter first number:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            b = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                c = a + b;
+                Console.WriteLine($"Addition of {a} and {b}, {c}");
+                d = a / b;
+                Console.WriteLine($"Div of {a} and {b}, {d}");
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("The second number should not be zero:");
+            }
+            finally
+            {
+                e = a * b;
+                Console.WriteLine($"Mul of {a} and {b},{e}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
