780cf9734bc9b9192da7571ff922fa0694303cb9
pavanchirra <pa1kumar.chirra@gmail.com>
2022-02-15 12:52:31 +0530

    Access Modifiers

---
+++ Day17 Assignment/Day17Project1/PavanLibrary/MyBaseClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace PavanLibrary
+{
+    /// <summary>
+    /// DONE.BY: PAVAN
+    /// PURPOSE: CREATING BASECLASS AND DERIVED CLASS
+    /// </summary>
+    public class MyBaseClass
+    {
+        public int a;
+        private int b;
+        protected int c;
+        internal int d;
+       protected internal int e;
+
+        public void MyBaseClassMethod()
+        {
+            a = 10;
+            b = 15;
+            c = 20;
+            d = 25;
+            e = 30;
+        }
+    }
+    public class MyDerivedClass : MyBaseClass
+    {
+        public void MyDerivedClassMethod()
+        {
+            a = 10;
+           // b = 15;   In this case Private class cannot be accessed//
+            c = 20;
+            d = 25;
+            e = 30;    
+        }
+    }
+    public class MyOtherClass
+    { 
+        public void MyOtherClassMethod()
+        {
+            MyBaseClass bc = new MyBaseClass();
+            bc.a = 10;
+          //  bc.b = 15;    Private Class is not accessed
+          //  bc.c = 20;    Protected Class is not accessed
+            bc.d = 25;
+            bc.e = 30;    
+        }
+    }
+}
---
+++ Day17 Assignment/Day17Project1/PublicLibrary/MyDerivedClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using PavanLibrary;
+
+namespace PublicLibrary
+{
+    /// <summary>
+    /// DONE BY: PAVAN
+    /// PURPOSE: INHERITING BASECLASS IN DERIVEDCLASS
+    /// </summary>
+    public class MyDerivedClass : MyBaseClass
+    {
+        public void MyDerivedClassMethod()
+        {
+            a = 10;
+          //  b = 15; Private Class cannot be accessed//
+            c = 20;
+           // d = 25;  internal class can't get access//
+            e = 30;  
+        }
+    }
+    public class MyDerivedOtherClass
+    {   
+        public void MyDerivedOtherClassMethod()
+        {
+            MyBaseClass bc = new MyBaseClass();
+            bc.a = 10;
+            //  bc.b = 15; Private class can't get access
+            //   bc.c = 20; Protected class can't get access
+            //  bc.d = 25; internal class can't get access
+            //  bc.e = 30; Protected internal class can't get access
+        }
+    }
+}
