0f2555ba27a33923592663881199106d4a4bb880
pavanchirra <pa1kumar.chirra@gmail.com>
2022-02-15 07:30:59 +0530

    Day16 Assignment

---
+++ Day16 Assignment/Day16Project(Partial class)/Day16Project(Partial class)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Pavan_Library;
+
+namespace Day16Project_Partial_class_
+{
+    /// <summary>
+    /// DONE BY: PAVAN
+    /// PURPOSE: CREATING A PARTIAL CLASS/
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****AIRTHEMATIC OPERATIONS****");
+            Console.WriteLine("Addition of 9 and 11 is: ");
+            Console.WriteLine(Mathematics.Add(9, 11)); 
+            Console.WriteLine("****Subtraction of 8 and 2 is****");
+            Console.WriteLine(Mathematics.Sub(8, 2));
+            Console.WriteLine("****Multiplication of 8 and 2 is****");
+            Console.WriteLine(Mathematics.Mul(8, 2));
+            Console.WriteLine("****Division of 8 and 2 is****");
+            Console.WriteLine(Mathematics.Div(8, 2));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Day16 Assignment/Day16Project(Partial class)/Pavan_Library/Mathematics2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pavan_Library
+{
+    public static partial class Mathematics
+    {
+        public static int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
