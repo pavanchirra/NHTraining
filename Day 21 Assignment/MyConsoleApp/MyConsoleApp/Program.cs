d71bc53550107ee237ba2f16de81af7dfa27120b
pavanchirra <pa1kumar.chirra@gmail.com>
2022-02-21 12:07:40 +0530

    WebServices

---
+++ Day 21 Assignment/MyConsoleApp/MyConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MyConsoleApp.ServiceReference1;
+
+namespace MyConsoleApp
+{
+    /// <summary>
+    /// DONE BY: PAVAN
+    /// PURPOSE: AIRTHEMATIC OPERATIONS USING WEB SERVICES
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebService1SoapClient obj = new WebService1SoapClient();
+            Console.WriteLine("****Factorial of a Number:***");
+            Console.WriteLine(obj.Factorial(4));
+            Console.WriteLine("****Addition of a Number:***");
+            Console.WriteLine(obj.Add(6, 2));
+            Console.WriteLine("***Subtraction of a Number:****");
+            Console.WriteLine(obj.Sub(6, 2));
+            Console.WriteLine("****Multiplication of a Number:*****");
+            Console.WriteLine(obj.Mul(6, 2));
+            Console.WriteLine("****Division of a Number:*****");
+            Console.WriteLine(obj.Div(6, 2));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day 21 Assignment/MyConsoleApp/MyWindowsApp/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using MyWindowsApp.ServiceReference1;
+
+namespace MyWindowsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebService1SoapClient obj = new WebService1SoapClient();
+                textBox2.Text = obj.Factorial(5).ToString();
+        }
+    }
+}
